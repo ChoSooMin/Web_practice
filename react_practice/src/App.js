dbc3f966141425872dfab72f001e8d60fc6aa39a
ChoSooMin <soom9611@gmail.com>
2021-01-05 11:11:17 +0900

    리액트_컴포넌트 만들어서 적용하기

--- react_practice/src/App.js
+++ react_practice/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import Hello from './component/Hello'; // 내가 만든 컴포넌트를 쉽게 사용하려면 import Hello 해야한다.
 
 function App() {
   return (
+    // 브라우저가 해석해야하는 부분 (JSX 문법)
+    // 반드시 root element는 있어야 한다.
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hello />
     </div>
   );
 }
 
-export default App;
+export default App; // export를 해야 다른 데에서 import를 할 수 ㅇㅆ다.
--- react_practice/src/index.js
+++ react_practice/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'; // App을 import해서 쓴다.
 import reportWebVitals from './reportWebVitals';
 
+// public/index.html의 id가 root인 곳에 App을 렌더링해서 넣는다.
+// document의 root 부분을 React.StrictMode로 렌더링하겠다.
 ReactDOM.render(
   <React.StrictMode>
     <App />
