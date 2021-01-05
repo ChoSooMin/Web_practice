55850e2d726517bd4d8729574c7d74a53663ad02
ChoSooMin <soom9611@gmail.com>
2021-01-05 14:23:40 +0900

    변수 타입 연습

---
+++ JS_practice/javascript/variableType.js
@@ -0,0 +1,12 @@
+// DataType의 종류
+// 1. primitive(int, float, char, boolean ... )
+// 2. Reference(Class, Array ... )
+let intV = 10;
+let floatV = 10.5;
+let stringV = "10";
+let booleanV = true;
+
+console.log(`intV = ${intV}`);
+console.log(`floatV = ${floatV}`);
+console.log(`stringV = ${stringV}`);
+console.log(`booleanV = ${booleanV}`);
