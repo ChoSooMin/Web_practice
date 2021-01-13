908ae66d90a4c4ad2d287b1b040664c2c47bb5da
ChoSooMin <soom9611@gmail.com>
2021-01-13 16:47:13 +0900

    모듈 사용 연습

--- python_pratice/module_basic/start.py
+++ python_pratice/module_basic/start.py
@@ -1,14 +1,15 @@
 # import test_module as test # test_module을 test라는 이름으로 사용하겠다 => ModuleNotFoundError 발생
-import test_package.test_module as test
-import test_package.test_module2 as test2
+# import test_package.test_module as test
+# import test_package.test_module2 as test2
+from test_package import *
 
-radius = test.number_input()
-print(test.get_circle_area(radius))
-print(test.get_circumference(radius))
+radius = test_module.number_input()
+print(test_module.get_circle_area(radius))
+print(test_module.get_circumference(radius))
 
-x, y = test2.number_input()
-print(test2.get_circumference(x, y))
-print(test2.get_rectangle_area(x, y))
+x, y = test_module2.number_input()
+print(test_module2.get_circumference(x, y))
+print(test_module2.get_rectangle_area(x, y))
 
 print("# 메인의 __name__ 출력 #")
 print(__name__) # 출력하면 __main__이라고 뜬다. -> 파이썬은 시작하는 파일을 entry point인 main으로 인식한다.
---
+++ python_pratice/module_basic/test_package/__init__.py
@@ -0,0 +1,2 @@
+# "from test_package import *"로 모듈을 읽어 들일 때 가져올 모듈
+__all__ = ["test_module", "test_module2"] # 사용시 읽어 들일 모듈의 목록
