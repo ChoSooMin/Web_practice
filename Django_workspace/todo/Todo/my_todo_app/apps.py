f475aaea5f68af2526052605466e3aa7a1f001df
ChoSooMin <soom9611@gmail.com>
2021-01-19 09:55:04 +0900

    Templates Composite View Pattern

--- Django_workspace/todo/Todo/my_todo_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyTodoAppConfig(AppConfig):
-    name = 'my_todo_app'
--- Django_workspace/todo/Todo/my_todo_app/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-# 클래스는 models.Model을 상속받는다
-class Todo(models.Model) :
-    objects = models.Manager()
-    content = models.CharField(max_length = 255)
